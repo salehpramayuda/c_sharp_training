c516ece9be0c5ed72ad0e9d43ddea8c9ad6fc25b
salehpramayuda <70847644+salehpramayuda@users.noreply.github.com>
2021-08-02 22:30:35 +0200

    first try with c#

---
+++ Training_Basic/Training_Basic/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Basic
+{
+    class Auto
+    {
+        private float Geschwindigkeit;
+
+        public Auto(float Geschwindigkeit)
+        {
+
+        }
+    }
+}
---
+++ Training_Basic/Training_Basic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Training_Basic
+{
+    class Program
+    {
+
+        /*
+         * Entry point into the program
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 1;
+            string s = "my age is ";
+            s = s + a;
+            string f = "abc";
+            ;
+            Console.WriteLine(f);
+
+            Console.Read();
+        }
+    }
+}
